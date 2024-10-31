22c573b0b1b4f01dabd40930d67632c61d99562b
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-10-31 09:58:38 +0100

    MainActivity: Header-Kommentar dazu

--- app/src/main/java/de/mide/android/bootstrap_app_in_webview/MainActivity.java
+++ app/src/main/java/de/mide/android/bootstrap_app_in_webview/MainActivity.java
@@ -6,19 +6,26 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+
+/**
+ * Diese Activity erzeugt programmatisch ein WebView-Element und zeigt darin
+ * eine Bootstrap-Webseite an, die im Verzeichnis "app/src/main/assets" liegt.
+ * Es wird hierdurch eine hybride App realisiert.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState ) {
 
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
 
         final WebView webView = new WebView( this );
         setContentView(webView);
 
         final WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptEnabled( true );
 
-        webView.loadUrl("file:///android_asset/index.html");
+        webView.loadUrl( "file:///android_asset/index.html" );
     }
+
 }
